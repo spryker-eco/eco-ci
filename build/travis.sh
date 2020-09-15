dee19d5acc7dfc2dcea6576f623f8ddb18da6ecc
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2020-09-15 17:50:29 +0300

    ECO-2952 build fix.

--- build/travis.sh
+++ build/travis.sh
@@ -12,22 +12,15 @@ if [[ *$TRAVIS_EVENT_TYPE* = 'cron' ]]; then git checkout $(git tag | tail -n 1)
 mkdir $MODULE_DIR
 ls -1 | grep -v ^$MODULE_DIR | grep -v ^ecoci | xargs -I{} mv {} $MODULE_DIR
 
-pwd
-echo "shop dir is $SHOP_DIR"
 echo "Cloning $PRODUCT_NAME..."
 git clone https://github.com/spryker-shop/$PRODUCT_NAME.git $SHOP_DIR
 cd $SHOP_DIR
-pwd
-echo "shop dir is $SHOP_DIR"
 
 composer global require hirak/prestissimo
 composer self-update && composer --version
 composer install --optimize-autoloader --no-interaction
 
-pwd
-echo "shop dir is $SHOP_DIR"
-
-nvm install 12
+nvm install 8
 
 mkdir -p shared/data/common/jenkins
 mkdir -p shared/data/common/jenkins/jobs
@@ -42,8 +35,6 @@ chmod -R a+x config/Shared/ci/travis/
 ./config/Shared/ci/travis/install_mailcatcher.sh
 ./config/Shared/ci/travis/configure_postgres.sh
 
-cat config/Shared/ci/travis/postgresql_ci.config >> config/Shared/config_local.php
-cp config/Shared/ci/travis/params_test_env.sh deploy/setup/params_test_env.sh
 cd ..
 
 chmod a+x ./ecoci/build/travis.sh
