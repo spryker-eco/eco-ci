481efc1e85cc58c848bbc76ed95b399b541790a3
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2020-09-15 16:11:06 +0300

    ECO-2952 build fix.

--- build/travis.sh
+++ build/travis.sh
@@ -12,15 +12,19 @@ if [[ *$TRAVIS_EVENT_TYPE* = 'cron' ]]; then git checkout $(git tag | tail -n 1)
 mkdir $MODULE_DIR
 ls -1 | grep -v ^$MODULE_DIR | grep -v ^ecoci | xargs -I{} mv {} $MODULE_DIR
 
+pwd
+echo "shop dir is $SHOP_DIR"
 echo "Cloning $PRODUCT_NAME..."
 git clone https://github.com/spryker-shop/$PRODUCT_NAME.git $SHOP_DIR
 cd $SHOP_DIR
+pwd
+echo "shop dir is $SHOP_DIR"
 
 composer global require hirak/prestissimo
 composer self-update && composer --version
 composer install --optimize-autoloader --no-interaction
 
-nvm install 8
+nvm install 12
 
 mkdir -p shared/data/common/jenkins
 mkdir -p shared/data/common/jenkins/jobs
@@ -31,11 +35,11 @@ chmod -R 777 data/
 chmod -R 660 config/Zed/dev_only_private.key
 chmod -R 660 config/Zed/dev_only_public.key
 chmod -R a+x config/Shared/ci/travis/
-./config/Shared/ci/travis/install_elasticsearch.sh
+./config/Shared/ci/travis/install_elasticsearch_6_8.sh
 ./config/Shared/ci/travis/install_mailcatcher.sh
+./config/Shared/ci/travis/configure_postgres.sh
 
-cat config/Shared/ci/travis/postgresql_ci.config >> config/Shared/ci/travis/config_ci.php
-cp config/Shared/ci/travis/config_ci.php config/Shared/config_local.php
+cat config/Shared/ci/travis/postgresql_ci.config >> config/Shared/config_local.php
 cp config/Shared/ci/travis/params_test_env.sh deploy/setup/params_test_env.sh
 cd ..
 
